52f6be7cc54e43d92e739f7563e360f59a91fb8d
inosilens <pashin.andrey94@gmail.com>
2021-11-20 19:39:17 +0300

    create new app

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.sass'
+import Main from "./pages/main/main";
+
+
+function App() {
+    return (
+        <>
+            <Main/>
+        </>
+    );
+}
+
+export default App;
